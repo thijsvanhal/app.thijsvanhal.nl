5e75e5be8fe4c3dca840ae6d7f13cdce22ff4fe8
Thijs van Hal <thijsvanhal7@gmail.com>
2023-02-13 17:04:56 +0100

    Vergelijkingsperiode toegevoegd
    - Toevoegen vergelijkingsperiode
    - Fix input css selector
    - Fix afbeeldingen op regex zoekwoorden

--- files/js/regex-builder-pagina-app.js
+++ files/js/regex-builder-pagina-app.js
@@ -54,6 +54,34 @@ function getLengthGA(regexga) {
     }
 }
 
+// Functie Date
+function getDate() {
+    const startDate = new Date(startDateInput.value);
+    const endDate = new Date(endDateInput.value);
+    const timeDiff = endDate - startDate;
+    const previousStartDate = new Date(startDate.getTime() - timeDiff - (1000 * 60 * 60 * 24));
+    const previousEndDate = new Date(endDate.getTime() - timeDiff - (1000 * 60 * 60 * 24));
+    const previousStartDateString = previousStartDate.toLocaleDateString('nl-NL', { day: '2-digit', month: '2-digit', year: 'numeric' });
+    const previousEndDateString = previousEndDate.toLocaleDateString('nl-NL', { day: '2-digit', month: '2-digit', year: 'numeric' });
+    if (document.documentElement.lang === 'nl') {
+        const DateOutputFirst = new String(`De vorige startdatum is ${previousStartDateString}`);
+        const DateOutputSecond = new String(`De vorige einddatum is ${previousEndDateString}`);
+        const resultDate = document.getElementById('date-text');
+        resultDate.innerHTML = `${DateOutputFirst}\n${DateOutputSecond}`;
+    } else if(document.documentElement.lang === 'en') {
+        const DateOutputFirst = new String(`The previous start date is ${previousStartDateString}`);
+        const DateOutputSecond = new String(`The previous end date is ${previousEndDateString}`);
+        const resultDate = document.getElementById('date-text');
+        resultDate.innerHTML = `${DateOutputFirst}\n${DateOutputSecond}`;
+    }
+    window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({'event': 'datum_gegenereerd'});
+}
+
+const startDateInput = document.getElementById("startDate");
+const endDateInput = document.getElementById("endDate");
+endDateInput.addEventListener("change", getDate);
+
 // Constanten
 const button = document.getElementById('generate-button');
 const textField = document.getElementById('input-area');
