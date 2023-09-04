a6bc626eb8aab69b174424ad661edffdf2a2c32e
Thijs van Hal <thijsvanhal7@gmail.com>
2023-09-04 11:44:08 +0200

    SERP Differences checker 3 zoekwoorden.
    de serp differences checker support nu ook 3 zoekwoorden!

--- files/js/serp-differences-app.js
+++ files/js/serp-differences-app.js
@@ -104,6 +104,7 @@ async function getData(login_storage, login, password, api_login) {
 
     const keyword1 = document.getElementById("zoekwoord-1").value;
     const keyword2 = document.getElementById("zoekwoord-2").value;
+    const keyword3 = document.getElementById("zoekwoord-3").value;
 
     mixedKeywordsArray = [];
     taskIds = [];
@@ -115,7 +116,13 @@ async function getData(login_storage, login, password, api_login) {
         {
             "keyword": keyword2
         }
-    ]
+    ];
+
+    if (keyword3) {
+        keywords.push({
+            "keyword": keyword3
+        });
+    }
 
     if (!api_login && login_storage === "") {
         window.alert('Je bent niet ingelogd! Log in en probeer het opnieuw!');
@@ -154,7 +161,11 @@ async function getData(login_storage, login, password, api_login) {
         const fetchPromises = taskIds.map(taskId => fetchData(taskId, login, password));
         const allResults = await Promise.all(fetchPromises);
         const keywordResults = allResults.map(taskResults => taskResults[0].result);
-        renderResults(keyword1, keyword2, keywordResults[0], keywordResults[1]);
+        if (keyword3) {
+            renderResults(keywords[0].keyword, keywords[1].keyword, keywords[2].keyword, keywordResults[0], keywordResults[1], keywordResults[2]);
+        } else {
+            renderResults(keywords[0].keyword, keywords[1].keyword, null, keywordResults[0], keywordResults[1], null);
+        }
     }
 }
 
@@ -183,29 +194,82 @@ async function fetchData(taskId, login, password) {
     return fetchResults;
 }
 
-async function renderResults(keyword1, keyword2, results1, results2) {
+async function renderResults(keyword1, keyword2, keyword3 = null, results1, results2, results3 = null) {
     const container = document.getElementById('serps');
 
-    const { similarityPercentage, commonResults } = calculateSimilarity(results1, results2);
+    const { similarityPercentage: similarityPercentage1, commonResults: commonResults1 } = calculateSimilarity(results1, results2);
 
-    container.innerHTML = `
-        <h2>SERP Resultaten</h2>
-        <p>De SERP's komen voor <strong>${similarityPercentage.toFixed(2)}%</strong> met elkaar overeen.</p>
-        <p>Overeenkomende URLs:</p>
-        <ul>
-            ${commonResults.map(result => `<li><a href="${result.url}">${result.url}</a></li>`).join('')}
-        </ul>
-        <div class="row">
-            <div class="col-sm" style="overflow-x: auto">
-                <h2>SERP 1: ${keyword1}</h2>
-                ${renderPositionResults(results1, commonResults)}
+    if (keyword3 != null) {
+        const { similarityPercentage: similarityPercentage, commonResults: commonResults } = calculateSimilarity(results1, results2, results3);
+        const { similarityPercentage: similarityPercentage2, commonResults: commonResults2 } = calculateSimilarity(results1, results3);
+        const { similarityPercentage: similarityPercentage3, commonResults: commonResults3 } = calculateSimilarity(results2, results3);
+        container.innerHTML = `
+            <h2>SERP Resultaten</h2>
+            <p>De 3 SERP's komen voor <strong>${similarityPercentage.toFixed(2)}%</strong> met elkaar overeen.</p>
+            <ul>
+                <li>SERP ${keyword1} en ${keyword2} komen voor <strong>${similarityPercentage1.toFixed(2)}%</strong> met elkaar overeen.</li>
+                <li>SERP ${keyword1} en ${keyword3} komen voor <strong>${similarityPercentage2.toFixed(2)}%</strong> met elkaar overeen.</li>
+                <li>SERP ${keyword2} en ${keyword3} komen voor <strong>${similarityPercentage3.toFixed(2)}%</strong> met elkaar overeen.</li>
+            </ul>
+            <h3>Overeenkomende URLs:</h3>
+            <ul>
+                ${commonResults.map(result => `<li><a href="${result.url}">${result.url}</a></li>`).join('')}
+            </ul>
+            <div class="row">
+                <div class="col-sm" style="overflow-x: auto">
+                    <h3>SERP ${keyword1} en ${keyword2}</h3>
+                    <ul>
+                        ${commonResults1.map(result => `<li><a href="${result.url}">${result.url}</a></li>`).join('')}
+                    </ul>
+                </div>
+                <div class="col-sm" style="overflow-x: auto">
+                    <h3>SERP ${keyword1} en ${keyword3}</h3>
+                    <ul>
+                        ${commonResults2.map(result => `<li><a href="${result.url}">${result.url}</a></li>`).join('')}
+                    </ul>
+                </div>
+                <div class="col-sm" style="overflow-x: auto">
+                    <h3>SERP ${keyword2} en ${keyword3}</h3>
+                    <ul>
+                        ${commonResults3.map(result => `<li><a href="${result.url}">${result.url}</a></li>`).join('')}
+                    </ul>
+                </div>
             </div>
-            <div class="col-sm" style="overflow-x: auto">
-                <h2>SERP 2: ${keyword2}</h2>
-                ${renderPositionResults(results2, commonResults)}
+            <div class="row">
+                <div class="col-sm" style="overflow-x: auto">
+                    <h2>SERP 1: ${keyword1}</h2>
+                    ${renderPositionResults(results1, commonResults1)}
+                </div>
+                <div class="col-sm" style="overflow-x: auto">
+                    <h2>SERP 2: ${keyword2}</h2>
+                    ${renderPositionResults(results2, commonResults2)}
+                </div>
+                <div class="col-sm" style="overflow-x: auto">
+                    <h2>SERP 3: ${keyword3}</h2>
+                    ${renderPositionResults(results3, commonResults3)}
+                </div>
             </div>
-        </div>
-    `;
+        `;
+    } else {
+        container.innerHTML = `
+            <h2>SERP Resultaten</h2>
+            <p>De SERP's komen voor <strong>${similarityPercentage1.toFixed(2)}%</strong> met elkaar overeen.</p>
+            <h3>Overeenkomende URLs:</h3>
+            <ul>
+                ${commonResults1.map(result => `<li><a href="${result.url}">${result.url}</a></li>`).join('')}
+            </ul>
+            <div class="row">
+                <div class="col-sm" style="overflow-x: auto">
+                    <h2>SERP 1: ${keyword1}</h2>
+                    ${renderPositionResults(results1, commonResults1)}
+                </div>
+                <div class="col-sm" style="overflow-x: auto">
+                    <h2>SERP 2: ${keyword2}</h2>
+                    ${renderPositionResults(results2, commonResults1)}
+                </div>
+            </div>
+        `;
+    }    
 }
 
 function renderPositionResults(results, commonResults) {
@@ -236,10 +300,11 @@ function renderPositionResults(results, commonResults) {
     }).join('');
 }
 
-function calculateSimilarity(results1, results2) {
+function calculateSimilarity(results1, results2, results3) {
+    console.log(results1, results2, results3);
     const allItems1 = results1.reduce((acc, result) => acc.concat(result.items), []);
     const allItems2 = results2.reduce((acc, result) => acc.concat(result.items), []);
-
+    
     const filteredItems1 = allItems1.filter(item => item.type !== "paid");
     const filteredItems2 = allItems2.filter(item => item.type !== "paid");
 
@@ -249,11 +314,30 @@ function calculateSimilarity(results1, results2) {
     const top10Items1 = filteredItems1.slice(0, 10);
     const top10Items2 = filteredItems2.slice(0, 10);
 
-    const commonResults = top10Items1.filter(item1 =>
-        top10Items2.some(item2 =>
-            item1.url === item2.url
-        )
-    );
+    if (results3 != null) {
+        const allItems3 = results3.reduce((acc, result) => acc.concat(result.items), []);
+        const filteredItems3 = allItems3.filter(item => item.type !== "paid");
+        filteredItems3.sort((a, b) => a.rank_absolute - b.rank_absolute);
+        const top10Items3 = filteredItems3.slice(0, 10);
+
+        const commonResults12 = top10Items1.filter(item1 =>
+            top10Items2.some(item2 =>
+                item1.url === item2.url
+            )
+        );
+    
+        var commonResults = commonResults12.filter(item1 =>
+            top10Items3.some(item3 =>
+                item1.url === item3.url
+            )
+        );
+    } else {
+        var commonResults = top10Items1.filter(item1 =>
+            top10Items2.some(item2 =>
+                item1.url === item2.url
+            )
+        );
+    }
 
     const similarityPercentage = (commonResults.length / 10) * 100;
 
